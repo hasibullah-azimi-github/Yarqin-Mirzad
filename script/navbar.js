ebd454fc458c7660e8b9bea4e06a420c3d616b96
Hasibullah Azimi <hasibullah.azimi99@gmail.com>
2023-10-24 21:20:04 +0430

    new style applied

---
+++ script/navbar.js
@@ -0,0 +1,59 @@
+
+
+
+        // Navbar Script Start
+
+        const navOpenBtn = document.querySelector(".navOpenBtn"),
+            navCloseBtn = document.querySelector(".navCloseBtn");
+
+        const navbar = document.querySelector(".sidebar");
+        const overlay = document.querySelector('.overlay');
+
+        navOpenBtn.addEventListener("click", () => {
+            navbar.classList.toggle("show-menu");
+            overlay.classList.toggle("show-overlay");
+        });
+
+        navCloseBtn.addEventListener("click", () => {
+            navbar.classList.toggle("show-menu");
+            overlay.classList.toggle("show-overlay");
+        });
+
+        overlay.addEventListener("click", () => {
+          navbar.classList.toggle("show-menu");
+          overlay.classList.toggle("show-overlay");
+      });
+
+
+        const menu = document.querySelector('.menu-item > div');
+        const menus = document.querySelector(".sub-menu");
+        const province = document.querySelectorAll(".province");
+        const district = document.querySelectorAll(".districts");
+
+
+        menu.addEventListener("click", () => {
+            menu.firstElementChild.classList.toggle("fa-plus");
+            menu.firstElementChild.classList.toggle("fa-minus")
+            menus.classList.toggle("show-menu");
+        })
+
+        province.forEach(((item, index) => {
+            item.addEventListener("click", () => {
+                district[index].classList.toggle("show-menu");
+                item.firstElementChild.classList.toggle("turn-angle")
+            });
+        }));
+
+
+
+
+        window.matchMedia("(max-width: 768px)").addEventListener("change", (viewPort)=>{
+        if (!viewPort.matches) {
+            if(navbar.classList.contains("show-menu") && overlay.classList.contains("show-overlay")){
+                navbar.classList.toggle("show-menu");
+                overlay.classList.toggle("show-overlay");
+            }
+          } 
+         });
+
+        // Navbar Script End
