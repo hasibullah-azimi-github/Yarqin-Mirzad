9a00824a4a05221901703c7694cdaf008897b8f9
Hasibullah Azimi <hasibullah.azimi99@gmail.com>
2023-11-25 17:22:13 +0430

    style applied

--- script/navbar.js
+++ script/navbar.js
@@ -1,67 +1,82 @@
+// Get body element
+const bodyElement = document.body;
+
+// Get modal element
+let modal = document.querySelector(".sidebar-modal");
+
+// Get open buttons
+const sidebarOpenbtn = document.querySelector(".navOpenBtn");
+
+// Get close button
+const sidebarClosebtn = document.querySelector(".navCloseBtn");
+
+// Listen for open click
+sidebarOpenbtn.addEventListener("click", () => {
+  bodyElement.style.overflow = 'hidden';
+  modal.classList.toggle("toggleshow");
+});
+
+sidebarClosebtn.addEventListener("click", () => {
+  bodyElement.style.overflow = 'auto';
+  modal.classList.toggle("toggleshow");
+});
+
+
+// Listen for outside click
+window.addEventListener("click", clickOutside);
+
+// Function to close modal if outside click
+function clickOutside(e) {
+  if (e.target == modal) {
+    bodyElement.style.overflow = 'auto';
+    modal.classList.toggle("toggleshow");
+  }
+}
+
+
+let navbar_menu = document.querySelectorAll(".menu-item  .section-links");
+
+// Function to close modal if menu click
+navbar_menu.forEach((menu) => {
+  menu.addEventListener("click", (e) => {
+    bodyElement.style.overflow = 'auto';
+    modal.classList.toggle("toggleshow");
+  });
+});
+
+
+// Function to close modal if sidebar remained open
+window.matchMedia("(max-width: 768px)").addEventListener("change", (viewPort) => {
+    if (!viewPort.matches) {
+      if (modal.classList.contains("toggleshow")) {
+        bodyElement.style.overflow = 'auto';
+        modal.classList.toggle("toggleshow");
+      }
+    }
+  });
 
 
 
-        // Navbar Script Start
-
-        const navOpenBtn = document.querySelector(".navOpenBtn"),
-            navCloseBtn = document.querySelector(".navCloseBtn");
-
-        const navbar = document.querySelector(".sidebar");
-        const overlay = document.querySelector('.overlay');
-
-        navOpenBtn.addEventListener("click", () => {
-            navbar.classList.toggle("show-menu");
-            overlay.classList.toggle("show-overlay");
-        });
-
-        navCloseBtn.addEventListener("click", () => {
-            navbar.classList.toggle("show-menu");
-            overlay.classList.toggle("show-overlay");
-        });
-
-        overlay.addEventListener("click", () => {
-          navbar.classList.toggle("show-menu");
-          overlay.classList.toggle("show-overlay");
-      });
 
 
-        const menu = document.querySelector('.menu-item > div');
-        const menus = document.querySelector(".sub-menu");
-        const province = document.querySelectorAll(".province");
-        const district = document.querySelectorAll(".districts");
+const menu = document.querySelector(".menu-item > div");
+const menus = document.querySelector(".sub-menu");
+const province = document.querySelectorAll(".province");
+const district = document.querySelectorAll(".districts");
 
+menu.addEventListener("click", () => {
+  menu.firstElementChild.classList.toggle("fa-plus");
+  menu.firstElementChild.classList.toggle("fa-minus");
+  menus.classList.toggle("toggleshow");
+});
 
-        menu.addEventListener("click", () => {
-            menu.firstElementChild.classList.toggle("fa-plus");
-            menu.firstElementChild.classList.toggle("fa-minus")
-            menus.classList.toggle("show-menu");
-        })
-
-        province.forEach(((item, index) => {
-            item.addEventListener("click", () => {
-                district[index].classList.toggle("show-menu");
-                item.firstElementChild.classList.toggle("turn-angle")
-            });
-        }));
+province.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    district[index].classList.toggle("toggleshow");
+    item.firstElementChild.classList.toggle("turn-angle");
+  });
+});
 
 
 
-
-        window.matchMedia("(max-width: 768px)").addEventListener("change", (viewPort)=>{
-        if (!viewPort.matches) {
-            if(navbar.classList.contains("show-menu") && overlay.classList.contains("show-overlay")){
-                navbar.classList.toggle("show-menu");
-                overlay.classList.toggle("show-overlay");
-            }
-          } 
-         });
-
-         let navbar_menu = document.querySelectorAll(".menu-item  .section-links");
-         navbar_menu.forEach(menu => {
-             menu.addEventListener("click", (e) =>{
-                navbar.classList.toggle("show-menu");
-                overlay.classList.toggle("show-overlay");
-             });
-         });
-
-        // Navbar Script End
+// Navbar Script End
